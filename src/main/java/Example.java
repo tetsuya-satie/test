a09db135c2816433935839df5992f22a16252b11
tetsuya_satie <tetsuyan1029@gmail.com>
2020-07-15 22:02:52 +0900

    add db-connection feature

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,14 +1,30 @@
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @EnableAutoConfiguration
 public class Example {
 
-    @RequestMapping("/")
-    String home() {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(path="/")
+    public String home() {
         return "Hello World!";
+    }
+
+    @RequestMapping(path="/users", method=RequestMethod.GET)
+    public String users(){
+        List<Map<String,Object>> users;
+        users = jdbcTemplate.queryForList("select * from users");
+        return users.toString();
     }
 
     public static void main(String[] args) {
