73df9a3824bbfa8adea3bde758140f0996e0357a
tetsuya_satie <tetsuyan1029@gmail.com>
2020-08-09 21:12:46 +0900

    fix login menu with Bootstrap

--- src/main/java/com/example/myproject/securingweb/WebSecurityConfig.java
+++ src/main/java/com/example/myproject/securingweb/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(WebSecurity web) throws Exception{
-    web.ignoring().antMatchers("/css/**", "/js/**");
+    web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
   }
 
 }
